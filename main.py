28d0b7e59386b0c4e3f386ab7e9fa2755eb4fc38
rupakshi1999 <rupakshiaggarwal2018@gmail.com>
2023-08-31 20:14:31 -0400

    add user login and authontication

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe, User
 from exts import db
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required
 
 # config the app
 app=Flask(__name__)
@@ -10,6 +13,12 @@ app.config.from_object(DevConfig)
 
 # register sqlalchemy to work with our app
 db.init_app(app)
+
+# add to database file without hacing to recreate it in cmd `echo $FLASK_APP`
+migrate=Migrate(app, db)
+
+# login 
+JWTManager(app)
 
 api=Api(app,doc='/docs')
 
@@ -24,10 +33,80 @@ recipe_model=api.model(
     }
 )
 
+signup_model=api.model(
+    # model {searializer tem[plate]}
+    "signup",
+    {
+        "username":fields.String(),
+        "email":fields.String(),
+        "password":fields.String()
+    }
+)
+
+login_model=api.model(
+    # model name {searializer tem[plate]}
+    "login",
+    {
+        "username":fields.String(),
+        "password":fields.String()
+    }
+)
+
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {"message":"Hello World"}
+    
+@api.route('/signup')
+class SignUp(Resource):
+
+    # show the format of data in /docs post
+    @api.expect(signup_model)
+    def post(self):
+        signup_data = request.get_json()
+
+        username=signup_data.get('username')
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} already exists"})
+        
+        email= signup_data.get('email')
+        db_email=User.query.filter_by(email=email).first()
+
+        if db_email is not None:
+            return jsonify({"message":f"Email {email} already exists"})
+
+
+        new_user = User(
+            username = signup_data.get('username'),
+            email = signup_data.get('email'),
+            password = generate_password_hash(signup_data.get('password'))
+        )
+
+        new_user.save()
+
+        return jsonify({"message":f"User {username} created successfully"})
+
+@api.route('/login')
+class Login(Resource):
+
+    @api.expect(login_model)
+    def post(self):
+        login_data=request.get_json()
+
+        username = login_data.get('username')
+        password = login_data.get('password')
+
+        db_user= User.query.filter_by(username=username).first()
+
+        if db_user and check_password_hash(db_user.password, password):
+            access_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
+        
+        return jsonify({"access token":access_token, "refresh token":refresh_token})
+
+
 
 @api.route('/recipes')
 class RecipesResource(Resource):
@@ -42,6 +121,8 @@ class RecipesResource(Resource):
         return recipes
 
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
+    @jwt_required()
     def post(self):
         """Create a new recipe"""
         # access data from json using request obeject that gets it from the frontend
@@ -67,6 +148,7 @@ class recipeResource(Resource):
         return recipe
     
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def put(self,id):
         """update a recipe"""
         recipe_to_update=Recipe.query.get_or_404(id)
@@ -78,6 +160,7 @@ class recipeResource(Resource):
         return recipe_to_update
 
     @api.marshal_with(recipe_model)
+    @jwt_required()
     def delete(self,id):
         """delete a recipe"""
         recipe_to_delete=Recipe.query.get_or_404(id)
--- models.py
+++ models.py
@@ -28,3 +28,25 @@ class Recipe(db.Model):
         self.title = title
         self.description = description
         db.session.commit()
+
+#user login info
+"""
+class User:
+    id:integer
+    username:string
+    email:string
+    password:string
+"""
+
+class User(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    username=db.Column(db.String(25), nullable=False, unique=True)
+    email=db.Column(db.String(80), nullable=False, unique=True)
+    password=db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
